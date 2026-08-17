2b095ea3a3b63ee6b1deeb51cd24df55fa169225
JUSOLI-0609 <sori7700@gmail.com>
2026-08-17 20:09:54 +0900

    Initial commit

---
+++ Source/Pawn_Character/Pawn_Character.cpp
@@ -0,0 +1,6 @@
+// Copyright Epic Games, Inc. All Rights Reserved.
+
+#include "Pawn_Character.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Pawn_Character, "Pawn_Character" );
